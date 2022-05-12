7eb51cc68e7874a9354a2809c11d2d616c4283fe
Latifah Nur Laila <100742938+latifahnl@users.noreply.github.com>
2022-05-12 17:05:37 +0700

    Add files via upload

---
+++ Operasi_Matematika.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#Function 
+def hello():
+    print("Hello")
+    
+def pertambahan(a, b):
+    return a+b
+def perkalian (a,b):
+    return a*b
+def pembagian (a,b):
+    return a/b
+def pengurangan (a,b):
+    return a-b
+
